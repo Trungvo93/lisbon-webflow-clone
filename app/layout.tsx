7cc6fea6d29f9167f6f008ad99baf1801897f4bc
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-08-28 16:39:15 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className} suppressHydrationWarning={true}>
-        <header className='fixed top-0 left-0 right-0 z-50'>
+        <header>
           <Header />
         </header>
         {/* Banner */}
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -12,50 +12,59 @@ const Header = () => {
     scrollElementToView(targetId);
   };
   return (
-    <div className='fixed top-0 w-full z-50'>
-      <div
-        className={`flex  justify-center h-[70px] bg-yellow-300  animate-opacity`}>
-        <div className='max-w-[1350px] w-full h-full flex justify-between items-center px-8'>
-          <Image
-            src='/assets/images/Lisbon Brand Logo.svg'
-            alt='Lisbon Logo'
-            width={104}
-            height={30}
-          />
-          <div className=' flex justify-between items-center  font-medium gap-[10px] '>
-            <Link
-              href={`#about`}
-              className='px-[15px] py-[7px] relative group '
-              onClick={(e) => {
-                handleScroll(e);
-              }}>
-              <span className=''>About</span>
-              <div className='absolute w-full h-full inset-0  bg-white rounded-[30px] opacity-0 group-hover:opacity-20 scale-75 group-hover:scale-100 duration-500'></div>
-            </Link>
-            <Link
-              href={`#banner`}
-              className='px-[15px] py-[7px] relative group'
-              onClick={(e) => {
-                handleScroll(e);
-              }}>
-              <span className=''>Speakers</span>
-              <div className='absolute w-full h-full inset-0  bg-white rounded-[30px] opacity-0 group-hover:opacity-20 scale-75 group-hover:scale-100 duration-500'></div>
-            </Link>
-            <Link href={`#`} className='px-[15px] py-[7px] relative group'>
-              <span className=''>Agenda</span>
-              <div className='absolute w-full h-full inset-0  bg-white rounded-[30px] opacity-0 group-hover:opacity-20 scale-75 group-hover:scale-100 duration-500'></div>
-            </Link>
-            <Link href={`#`} className='px-[23px] py-[7px] relative group'>
-              <Image
-                src='/assets/images/Ticket Icon.svg'
-                alt='Ticket Icon'
-                width={20}
-                height={0}
-                className='object-cover'
-              />
-              <div className='absolute w-full h-full inset-0  bg-white rounded-[30px] opacity-0 group-hover:opacity-20 scale-75 group-hover:scale-100 duration-500'></div>
-            </Link>
-          </div>
+    <div
+      className={` fixed top-0 w-full z-50 flex  justify-center h-[70px] bg-black animate-opacity`}>
+      <div className='max-w-[1350px] w-full h-full flex justify-between items-center px-8'>
+        <Image
+          src='/assets/images/Lisbon Brand Logo.svg'
+          alt='Lisbon Logo'
+          width={104}
+          height={30}
+        />
+        <div className=' flex justify-between items-center  font-medium gap-[10px] '>
+          <Link
+            href={`#about`}
+            className='px-[15px] py-[7px] relative group laptop:block hidden '
+            onClick={(e) => {
+              handleScroll(e);
+            }}>
+            <span className=''>About</span>
+            <div className='absolute w-full h-full inset-0  bg-white rounded-[30px] opacity-0 group-hover:opacity-20 scale-75 group-hover:scale-100 duration-500'></div>
+          </Link>
+          <Link
+            href={`#banner`}
+            className='px-[15px] py-[7px] relative group laptop:block hidden'
+            onClick={(e) => {
+              handleScroll(e);
+            }}>
+            <span className=''>Speakers</span>
+            <div className='absolute w-full h-full inset-0  bg-white rounded-[30px] opacity-0 group-hover:opacity-20 scale-75 group-hover:scale-100 duration-500'></div>
+          </Link>
+          <Link
+            href={`#`}
+            className='px-[15px] py-[7px] relative group laptop:block hidden'>
+            <span className=''>Agenda</span>
+            <div className='absolute w-full h-full inset-0  bg-white rounded-[30px] opacity-0 group-hover:opacity-20 scale-75 group-hover:scale-100 duration-500'></div>
+          </Link>
+          <Link href={`#`} className='px-[23px] py-[7px] relative group '>
+            <Image
+              src='/assets/images/Ticket Icon.svg'
+              alt='Ticket Icon'
+              width={20}
+              height={0}
+              className='object-cover'
+            />
+            <div className='absolute w-full h-full inset-0  bg-white rounded-[30px] opacity-0 group-hover:opacity-20 scale-75 group-hover:scale-100 duration-500'></div>
+          </Link>
+          <button className='bg-white w-[42px] h-[42px] rounded-xl flex justify-center items-center '>
+            <Image
+              src='assets/images/menu-icon.svg'
+              alt='menu icon'
+              width={22}
+              height={14}
+              className='object-cover '
+            />
+          </button>
         </div>
       </div>
     </div>
